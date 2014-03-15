a821d962a25d5bab4abb7063f3f34a173a4a4944
Anders Hefre <anders.martinsen@gmail.com>
2014-03-15 22:04:18 +0100

    Fått til enkel forflytning med tastaturet av ballen.

--- snake/web/snake.dart
+++ snake/web/snake.dart
@@ -13,23 +13,49 @@ class Snake {
   int x, y, r;
   int dx = 2;
   int dy = 4;
-  
+  bool rightDown = false;
+  bool leftDown = false;
+    
   Snake(this.board, this.x, this.y, this.r) {
     draw();
-  }
+    document.onKeyDown.listen(_onKeyDown);
+    document.onKeyUp.listen(_onKeyUp);
+   }
   
   void draw() {
+    if (rightDown) {
+       x += dx;
+       y -= dy;
+    } else if (leftDown) {
+       x -= dx;
+       y -= dy;
+    }
+
     board.context.beginPath();
     board.context.arc(x, y, r, 0, PI*2, true);
     board.context.closePath();
     board.context.fill();    
   }
   
+  _onKeyDown(event) {
+    if (event.keyCode == 39) { 
+      rightDown = true;
+    } else if (event.keyCode == 37) {
+      leftDown = true;
+    }
+  }
+  
+  _onKeyUp(event) {
+    if (event.keyCode == 39) {
+      rightDown = false;
+    } else if (event.keyCode == 37) {
+      leftDown = false;
+    }
+  }
+  
   void move() {
       board.clear();
       draw();
-      x += dx;
-      y += dy;
   }
 }
 
@@ -38,7 +64,7 @@ class Board {
     int width, height;
     
     void init() {
-      Snake snake = new Snake(this, 0, 0, 10); 
+      Snake snake = new Snake(this, 0, 300, 10); 
       new Timer.periodic(const Duration(milliseconds: 50),
                  (t) => snake.move());
     }
